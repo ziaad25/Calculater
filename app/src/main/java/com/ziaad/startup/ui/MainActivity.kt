c05fa413024c9edbb73d3f89a2f783da6280df7c
Ziad Abo elela <ziaadaboelela@gmail.com>
2023-03-13 13:17:24 +0200

    design of calculater

---
+++ app/src/main/java/com/ziaad/startup/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ziaad.startup.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ziaad.startup.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
