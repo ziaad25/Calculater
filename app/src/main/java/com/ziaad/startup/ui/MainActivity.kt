15f9a6a225fbc6cdbc02a917400194b5e4be90fd
Ziad Abo elela <ziaadaboelela@gmail.com>
2023-03-14 03:53:15 +0200

    optmize the logic and design

--- app/src/main/java/com/ziaad/startup/ui/MainActivity.kt
+++ app/src/main/java/com/ziaad/startup/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ziaad.startup.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,46 +11,46 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btnHex: Button
-    lateinit var btnOctal: Button
-    lateinit var btnDec: Button
-    lateinit var btnBin: Button
-    lateinit var tvInput: TextView
-    lateinit var btnEqual: Button
-    lateinit var btnDel: Button
-    lateinit var btnClear: Button
+    private lateinit var btnHex: Button
+    private lateinit var btnOctal: Button
+    private lateinit var btnDec: Button
+    private lateinit var btnBin: Button
+    private lateinit var screenView: TextView
+    private lateinit var btnEqual: Button
+    private lateinit var btnDel: Button
+    private lateinit var btnClear: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        intinalize()
-        decimal()
+        initialize()
+        enableDecimalOperation()
 
         btnDec.setOnClickListener {
             default()
-            decimal()
+            enableDecimalOperation()
         }
 
         btnOctal.setOnClickListener {
             default()
-            octal()
+            enableOctalButtonOperation()
         }
 
         btnHex.setOnClickListener {
             default()
-            hexa()
+            enableHexaButtonOperation()
         }
         btnBin.setOnClickListener {
             default()
-            binary()
+            enableBinaryButtonOperation()
         }
 
 
     }
 
-    fun intinalize() {
-        tvInput = findViewById(R.id.tv_Input)
+    private fun initialize() {
+        screenView = findViewById(R.id.tv_Input)
         btnHex = findViewById(R.id.btnHex)
         btnOctal = findViewById(R.id.btnOctal)
         btnDec = findViewById(R.id.btnDec)
@@ -61,8 +60,8 @@ class MainActivity : AppCompatActivity() {
         btnClear = findViewById(R.id.btnClear)
     }
 
-    fun default() {
-        tvInput.text = ""
+    private fun default() {
+        screenView.text = ""
         tv_ResultHex.text = "0"
         tv_ResultBinary.text = "0"
         tv_ResultDec.text = "0"
@@ -70,84 +69,84 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickNumber(view: View) {
-        var numDigit = (view as Button).text.toString()
-        var oldNum = tvInput.text.toString()
-        var newNum = oldNum + numDigit
-        tvInput.text = newNum
+        val numDigit = (view as Button).text.toString()
+        val oldNum = screenView.text.toString()
+        val newNum = oldNum + numDigit
+        screenView.text = newNum
     }
 
     fun onClick(view: View) {
         when (view) {
-            btnClear -> tvInput.text = ""
+            btnClear -> screenView.text = ""
             btnDel -> {
-                var num = tvInput.text.toString()
-                var newNum = num.dropLast(1)
-                tvInput.text = newNum
+                val num = screenView.text.toString()
+                val newNum = num.dropLast(1)
+                screenView.text = newNum
             }
             btnEqual -> {
                 if (btnDec.isSelected) {
-                    decInput()
+                    inputOfDec()
                 } else if (btnHex.isSelected) {
-                    hexInput()
+                    inputOfHexa()
                 } else if (btnOctal.isSelected) {
-                    octalInput()
+                    inputOfOctal()
                 } else if (btnBin.isSelected) {
-                    binaryInput()
+                    inputOfBinary()
                 }
             }
         }
     }
 
-    fun decimal() {
+    private fun enableDecimalOperation() {
         disSelectedButtons(btnHex, btnOctal, btnBin)
         enableButton(btnHex, btnOctal, btnBin)
         btnDec.isSelected = true
         btnDec.isEnabled = false
         disableAlpha()
-        enableNum()
-        decInput()
+        enableAllNumber()
+        inputOfDec()
 
     }
 
-    fun binary() {
+    private fun enableBinaryButtonOperation() {
         disSelectedButtons(btnHex, btnOctal, btnDec)
         enableButton(btnHex, btnOctal, btnDec)
         btnBin.isEnabled = false
         btnBin.isSelected = true
         disableAlpha()
-        disbleNum()
-        binaryInput()
+        enableBinaryNumber()
+        inputOfBinary()
     }
 
-    fun hexa() {
+    private fun enableHexaButtonOperation() {
         enableButton(btnBin, btnOctal, btnDec)
         disSelectedButtons(btnBin, btnDec, btnOctal)
         btnHex.isEnabled = false
         btnHex.isSelected = true
         enableAlpha()
-        enableNum()
-        hexInput()
+        enableAllNumber()
+        inputOfHexa()
     }
 
-    fun octal() {
+    private fun enableOctalButtonOperation() {
         disSelectedButtons(btnDec, btnBin, btnHex)
         enableButton(btnHex, btnBin, btnDec)
         btnOctal.isEnabled = false
         btnOctal.isSelected = true
         disableAlpha()
-        enableNum()
-        octalInput()
+        enableAllNumber()
+        inputOfOctal()
     }
 
-    fun disableAlpha() {
+    private fun disableAlpha() {
         disableButton(btnA, btnB, btnC, btnD, btnE, btnF)
     }
 
-    fun enableAlpha() {
+    private fun enableAlpha() {
         enableButton(btnA, btnB, btnC, btnD, btnE, btnF)
     }
 
-    fun enableNum() {
+    private fun enableAllNumber() {
         enableButton(btnNum2, btnNum3, btnNum4, btnNum5, btnNum6, btnNum7, btnNum8, btnNum9)
         if (btnOctal.isSelected) {
             btnNum8.isEnabled = false
@@ -155,67 +154,67 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun disbleNum() {
+    private fun enableBinaryNumber() {
         disableButton(btnNum2, btnNum3, btnNum4, btnNum5, btnNum6, btnNum7, btnNum8, btnNum9)
     }
 
 
-    fun decInput() {
-        if (tvInput.text.isNotEmpty()) {
-            var num = tvInput.text.toString()
+    private fun inputOfDec() {
+        if (screenView.text.isNotEmpty()) {
+            val num = screenView.text.toString()
 
-            var dec = num.toLong()
+            val dec = num.toLong()
 
-            var hex = dec.toULong().toString(16)
-            var octal = dec.toULong().toString(8)
-            var bin = dec.toULong().toString(2)
+            val hex = dec.toULong().toString(16)
+            val octal = dec.toULong().toString(8)
+            val bin = dec.toULong().toString(2)
 
-            showInResult(dec.toString(), hex, octal, bin)
+            showResult(dec.toString(), hex, octal, bin)
         }
     }
 
-    fun hexInput() {
-        if (tvInput.text.isNotEmpty()) {
-            var num = tvInput.text.toString()
+    private fun inputOfHexa() {
+        if (screenView.text.isNotEmpty()) {
+            val num = screenView.text.toString()
 
-            var dec: Long = Integer.parseInt(num, 16).toLong()
-            var hex = dec.toULong().toString(16)
-            var bin = dec.toULong().toString(2)
-            var octal = dec.toULong().toString(8)
+            val dec: Long = Integer.parseInt(num, 16).toLong()
+            val hex = dec.toULong().toString(16)
+            val bin = dec.toULong().toString(2)
+            val octal = dec.toULong().toString(8)
 
-            showInResult(dec.toString(), hex, octal, bin)
-        }
-    }
-
-
-    fun octalInput() {
-        if (tvInput.text.isNotEmpty()) {
-            var num = tvInput.text.toString()
-
-            var dec = Integer.parseInt(num, 8).toLong()
-            var hex = dec.toULong().toString(16)
-            var bin = dec.toULong().toString(2)
-            var octal = dec.toULong().toString(8)
-
-            showInResult(dec.toString(), hex, octal, bin)
-        }
-    }
-
-    fun binaryInput() {
-        if (tvInput.text.isNotEmpty()) {
-            var num = tvInput.text.toString()
-
-            var dec = Integer.parseInt(num, 2).toLong()
-            var hex = dec.toULong().toString(16)
-            var octal = dec.toULong().toString(8)
-            var bin = dec.toULong().toString(2)
-
-            showInResult(dec.toString(), hex, octal, bin)
+            showResult(dec.toString(), hex, octal, bin)
         }
     }
 
 
-    fun showInResult(dec: String, hex: String, octal: String, bin: String) {
+    private fun inputOfOctal() {
+        if (screenView.text.isNotEmpty()) {
+            val num = screenView.text.toString()
+
+            val dec = Integer.parseInt(num, 8).toLong()
+            val hex = dec.toULong().toString(16)
+            val bin = dec.toULong().toString(2)
+            val octal = dec.toULong().toString(8)
+
+            showResult(dec.toString(), hex, octal, bin)
+        }
+    }
+
+    private fun inputOfBinary() {
+        if (screenView.text.isNotEmpty()) {
+            val num = screenView.text.toString()
+
+            val dec = Integer.parseInt(num, 2).toLong()
+            val hex = dec.toULong().toString(16)
+            val octal = dec.toULong().toString(8)
+            val bin = dec.toULong().toString(2)
+
+            showResult(dec.toString(), hex, octal, bin)
+        }
+    }
+
+
+    private fun showResult(dec: String, hex: String, octal: String, bin: String) {
         tv_ResultBinary.text = bin
         tv_ResultDec.text = dec
         tv_ResultOctal.text = octal
